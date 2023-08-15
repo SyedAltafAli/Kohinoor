3bad1db13ec3252968ab6462235c32b88f012e61
SyedAltafAli <97794157+SyedAltafAli@users.noreply.github.com>
2023-08-15 21:03:06 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # managing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -131,5 +131,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static") 
+    os.path.join(BASE_DIR, "staticfiles") 
 ]
